f6f90c80f318940a6734206aebe96628a0b32216
Mo Hon Sing <h.s.mo@oocl.com>
2018-11-29 16:34:33 +0800

    modify employee

--- src/main/java/com/tw/apistackbase/core/Employee.java
+++ src/main/java/com/tw/apistackbase/core/Employee.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Employee {
@@ -12,6 +13,17 @@ public class Employee {
     private Long id;
     private String name;
     private int age;
+
+    @ManyToOne()
+    private Company company;
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 
     public Employee() {}
 
